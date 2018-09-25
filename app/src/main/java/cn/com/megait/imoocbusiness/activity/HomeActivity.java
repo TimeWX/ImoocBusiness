c927935f042ca11834906c69d72090c08b86618c
wanxiang5363 <48038614@qq.com>
2018-09-25 17:01:40 +0800

    首页编写

--- app/src/main/java/cn/com/megait/imoocbusiness/activity/HomeActivity.java
+++ app/src/main/java/cn/com/megait/imoocbusiness/activity/HomeActivity.java
@@ -1,15 +1,41 @@
 package cn.com.megait.imoocbusiness.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cn.com.megait.imoocbusiness.R;
+import cn.com.megait.imoocbusiness.view.fragment.home.HomeFragment;
+import cn.com.megait.imoocbusiness.view.fragment.home.MessageFragment;
+import cn.com.megait.imoocbusiness.view.fragment.home.MineFragment;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private FragmentManager fm;
+    private HomeFragment mHomeFragment;
+    private Fragment mCommonFragmentOne;
+    private MessageFragment mMessageFragment;
+    private MineFragment mMineFragment;
+    private Fragment mCurrent;
+
+    @BindView(R.id.home_layout_view) RelativeLayout mHomeLayout;
+    @BindView(R.id.pond_layout_view) RelativeLayout mPondLayout;
+    @BindView(R.id.message_layout_view) RelativeLayout mMessageLayout;
+    @BindView(R.id.mine_layout_view) RelativeLayout mMineLayout;
+    @BindView(R.id.home_image_view) TextView mHomeView;
+    @BindView(R.id.fish_image_view) TextView mPondView;
+    @BindView(R.id.message_image_view) TextView mMessageView;
+    @BindView(R.id.mine_image_view) TextView mMineView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home_layout);
+        ButterKnife.bind(this);
     }
 }
--- app/src/main/java/cn/com/megait/imoocbusiness/view/fragment/BaseFragment.java
+++ app/src/main/java/cn/com/megait/imoocbusiness/view/fragment/BaseFragment.java
@@ -10,49 +10,65 @@ import cn.com.megait.imoocbusiness.constant.Constant;
 
 /**
  * @author TimeW
- * @function 所有Fragment基类,权限的申请,判断
- *
+ * @function 所有Fragment基类, 权限的申请, 判断
  */
 public class BaseFragment extends Fragment {
 
     /**
      * 申请指定权限
+     *
      * @param code
      * @param permissions
      */
-    public void requestPermission(int code,String... permissions){
-        if(Build.VERSION.SDK_INT>=23){
-            requestPermissions(permissions,code);
+    public void requestPermission(int code, String... permissions) {
+        if (Build.VERSION.SDK_INT >= 23) {
+            requestPermissions(permissions, code);
         }
     }
 
     /**
      * 判断是否有指定权限
+     *
      * @param permissions
      * @return
      */
-    public boolean hasPermission(String... permissions){
-        for(String permission:permissions){
+    public boolean hasPermission(String... permissions) {
+        for (String permission : permissions) {
             //判断权限是否被授权
-            if(ContextCompat.checkSelfPermission(getActivity(),permission)!= PackageManager.PERMISSION_GRANTED){
-                return  false;
+            if (ContextCompat.checkSelfPermission(getActivity(), permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
             }
         }
         return true;
     }
 
+    /**
+     * 权限授权与否的回调结果
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode){
-
+        switch (requestCode) {
+            //请求读写内存
             case Constant.WRITE_READ_EXTERNAL_CODE:
+                if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    doWriteSDCard();
+                }
                 break;
-                case Constant.HARDWARE_CAMERA_CODE:
-                    break;
+            //请求打开照相机
+            case Constant.HARDWARE_CAMERA_CODE:
+                if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    doOpenCamera();
+                }
+                break;
         }
     }
 
-    public void doOpenCamera(){}
+    public void doOpenCamera() {
+    }
 
-    public void doWriteSDCard(){}
+    public void doWriteSDCard() {
+    }
 }
