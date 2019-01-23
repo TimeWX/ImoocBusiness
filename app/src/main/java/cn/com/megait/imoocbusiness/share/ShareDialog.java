de7f019be497f5d3fe378239325b38ece51e42d9
wanxiang5363 <48038614@qq.com>
2019-01-23 16:57:04 +0800

    1.23

--- app/src/main/java/cn/com/megait/imoocbusiness/share/ShareDialog.java
+++ app/src/main/java/cn/com/megait/imoocbusiness/share/ShareDialog.java
@@ -5,9 +5,13 @@ import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
 
+import butterknife.BindView;
 import butterknife.OnClick;
 import cn.com.megait.imoocbusiness.R;
 
@@ -17,6 +21,9 @@ import cn.com.megait.imoocbusiness.R;
  * @date 2019/1/22
  */
 public class ShareDialog extends Dialog implements View.OnClickListener {
+
+    @BindView(R.id.rl_download_layout)
+    RelativeLayout rlDownloadLayout;
 
     private Context mContext;
     private boolean isShowDownload;
@@ -42,16 +49,24 @@ public class ShareDialog extends Dialog implements View.OnClickListener {
     }
 
     private void initView() {
-        Window dialogWindow=getWindow();
-        /**
-         * TODO Now-Coding
-         */
+        Window dialogWindow = getWindow();
+        dialogWindow.setGravity(Gravity.BOTTOM);
+        WindowManager.LayoutParams lp = dialogWindow.getAttributes();
+        lp.width = mDisplayMetrics.widthPixels;//设置宽度
+        dialogWindow.setAttributes(lp);
+        if (isShowDownload) {
+            rlDownloadLayout.setVisibility(View.VISIBLE);
+        }
+
     }
 
     @OnClick({R.id.rl_weixin_layout, R.id.rl_moment_layout, R.id.rl_qq_layout, R.id.rl_qqzone_layout, R.id.rl_download_layout, R.id.tv_cancel})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.rl_weixin_layout:
+                /**
+                 * TODO Now-Coding
+                 */
                 break;
             case R.id.rl_moment_layout:
                 break;
