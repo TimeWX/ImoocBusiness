3253025afcd163132eeb837a6dc2362d6dad4fb0
wanxiang5363 <48038614@qq.com>
2019-01-25 16:54:03 +0800

    1.25

---
+++ app/src/main/java/cn/com/megait/imoocbusiness/share/ShareManager.java
@@ -0,0 +1,12 @@
+package cn.com.megait.imoocbusiness.share;
+
+/**
+ * @author lenovo
+ * @function
+ * @date 2019/1/25
+ */
+public class ShareManager {
+    private static ShareManager instance=null;
+
+
+}
